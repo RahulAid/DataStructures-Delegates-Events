c7f0949a0bd3bc7e90ca03f50d3440a2fe8c1109
RahulAid <raccess97@gmail.com>
2022-11-20 22:53:35 +0530

    Single and Multicast Delegates

---
+++ MulticastDelegates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class MulticastDelegates
+{
+    delegate void MultipleOperation(int x, int y);
+
+    public static void ImplementMulticastDelegates()
+    {
+        Console.WriteLine("\nMulticast Delegates");
+        MultipleOperation objMultiDel = Operation.Add;
+        objMultiDel += Operation.Sub;
+
+        objMultiDel(25, 15);
+        objMultiDel(25, 15);
+    }
+
+    public class Operation
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine($"\nAddition Operation : {a + b}");
+        }
+
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine($"\nSubtraction Operation : {a - b}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructures_Delegates_Events
+{
+    public delegate int Operation(int x, int y);
+    public class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delegates\n");
+            Operation obj1 = new Operation(Addition);
+            Operation obj2 = new Operation(Subtraction);
+
+            Console.WriteLine($"\nAddition of two numbers : {obj1(25, 15)}");
+            Console.WriteLine($"\nSubtraction of two numbers : {obj2(25, 15)}");
+
+            MulticastDelegates.ImplementMulticastDelegates();
+
+            Console.ReadLine();
+        }
+    }
+}
